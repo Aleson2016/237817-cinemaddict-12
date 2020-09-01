5069403af302f053cb74211c09e653d6f0adbaac
Aleks <alekssson@gmail.com>
2020-09-01 18:18:01 +0300

    Установка webpack и деление на модули

---
+++ src/view/films-extra-list.js
@@ -0,0 +1,11 @@
+export const createFilmsExtraListTemplate = (title) => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title">${title}</h2>
+
+      <div class="films-list__container">
+
+      </div>
+    </section>`
+  );
+};
